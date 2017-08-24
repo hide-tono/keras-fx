3f17005bf0d1a69a800e09613bfb6d181582b363
hide-tono <information@tono-n-chi.com>
2017-08-24 16:01:00 +0900

    出力をsoftmax、誤差関数をcrossentropyに変更

--- make_train_data.py
+++ make_train_data.py
@@ -1,5 +1,5 @@
-import pandas
 import numpy
+import pandas
 
 df = pandas.read_csv('EURUSD_M1_201706_with_signal.csv')
 df.drop('datetime', axis=1)
@@ -15,7 +15,16 @@ df.drop('signal', axis=1)
 for i in range(1440, len(df), 60):
     # とりあえず終値だけにしてみる
     append_x = numpy.array([df['c'].ix[i - maxlen: i - 1]])
-    append_y = numpy.array([signal.ix[i]])
+    append_y = numpy.array([0, 0, 0])
+
+    # 1-of-K 表現に変換
+    if signal.ix[i] < 0:
+        append_y[0] = 1
+    elif signal.ix[i] > 0:
+        append_y[2] = 1
+    else:
+        append_y[1] = 1
+
     x = numpy.append(x, append_x)
     y = numpy.append(y, append_y)
 
--- predict_rnn.py
+++ predict_rnn.py
@@ -15,18 +15,21 @@ maxlen = 1440  # ひとつの時系列データの長さ(24時間)
 x = numpy.load('EURUSD_M1_201706_x.npy')
 y = numpy.load('EURUSD_M1_201706_y.npy')
 
+# 正規化
+x = x / numpy.linalg.norm(x)
+
 x = x.reshape((int(len(x) / maxlen), maxlen, 1))
-y = y.reshape((len(x), 1))
+y = y.reshape((len(x), 3))
 
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, train_size=0.8)
 X_train, X_validation, Y_train, Y_validation = \
     train_test_split(X_train, Y_train, train_size=0.8)
 
 n_in = len(x[0][0])  # 1440
-n_hidden = 30
-n_out = len(y[0])  # 1
+n_hidden = 16
+n_out = len(y[0])  # 3
 
-epochs = 500
+epochs = 10
 batch_size = 10
 
 early_stopping = EarlyStopping(monitor='loss', patience=100, verbose=1)
@@ -35,13 +38,12 @@ model = Sequential()
 model.add(GRU(n_hidden,
               kernel_initializer=weight_variable,
               input_shape=(maxlen, n_in)))
-model.add(Dense(n_out, kernel_initializer=weight_variable))
-model.add(Activation('linear'))
+model.add(Dense(n_out))
+model.add(Activation('softmax'))
 
 optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999)
-model.compile(loss='mean_squared_error',
+model.compile(loss='categorical_crossentropy',
               optimizer=optimizer)
-
 
 hist = model.fit(X_train, Y_train,
                  batch_size=batch_size,
